3d381bd85553a7827c8e1f44e4e8b1515510f33d
CodeSandbox <support@codesandbox.io>
2023-08-10 00:18:47 +0000

    Sandbox snapshot commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import Interview from "./component/Interview";
+export default function App() {
+  return (
+    <div className="App">
+       <Interview/>
+    </div>
+  );
+}
---
+++ src/component/Interview.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+// import "./Interview.css";
+const Interview = () => {
+  return (
+    <div>
+        <div>
+        <h1>Manish Nigam</h1>
+        <p>I am a Fronted Devloper</p>
+        <a href="https://www.linkedin.com/in/manish-nigam-2a3328163/" >LinkedIn</a>
+        <a href="https://github.com/">Github</a>
+        </div>
+        <hr />
+        <div>
+            <h1>Experience</h1>
+            <h4>Fronted Devloper at Simpplr</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae consequuntur quia non quisquam ad similique facere doloremque minima, sapiente magni, in sunt recusandae amet at veritatis earum aliquam quidem iusto!</p>
+        </div>
+        <hr />
+        <div>
+            <h4>Training at Geekster</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure consectetur animi nisi fugiat quasi placeat tenetur perspiciatis vel, earum itaque laudantium molestias at cupiditate cumque quae ex rerum illum facere ipsum. Cupiditate dolore fugit facilis, hic sit odit velit ex, tempora ratione nulla asperiores fuga voluptas. Enim est ipsam quis placeat eaque? Fugit distinctio maxime adipisci aspernatur in, commodi cumque?</p>
+        </div>
+        <hr />
+        <div>
+            <h1>Skills</h1>
+            <ul>
+                <li>Java</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>ReactJs</li>
+            </ul>
+        </div>
+        <hr />
+        <div>
+            <h1>Hobbies</h1>
+            <ol>
+                <li>Playing Cricket</li>
+                <li>See Webseries</li>
+                <li>See Movies</li>
+                <li>Playing Card</li>
+            </ol>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Interview
